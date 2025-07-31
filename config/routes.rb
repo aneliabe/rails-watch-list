5c307013a920f5bb86a7dbfdd669b4627a2f4fa4
Aneli Abe <abe.aneli@gmail.com>
2025-07-31 20:43:10 -0300

    fixing lists create

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  resources :lists, only: %i[index new post show] do
+  # get "up" => "rails/health#show", as: :rails_health_check
+  resources :lists, only: %i[index new create show] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[show destroy]
